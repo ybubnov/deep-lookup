a8437878e5d6881052e1d328449148e4781ebf1c
Yasha Bubnov <ybubnov@lyft.com>
2021-05-08 20:04:09 +0300

    Efficient Rsamp.

--- deeplookup/ts.py
+++ deeplookup/ts.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from typing import NamedTuple
 
 import numpy as np
-from numpy.linalg import norm
 
 
 class N(NamedTuple):
@@ -15,41 +14,12 @@ class N(NamedTuple):
     ts: int = 0
 
 
-def from_prob_linear(xx, ts):
-    probs = list(zip(xx, ts))
-    nodes0 = [N(f"a{i}", prob[0], klass=0, ts=prob[1]) for i, prob in enumerate(probs)]
-    nodes1 = [N(f"b{i}", prob[0], klass=1, ts=prob[1]) for i, prob in enumerate(probs)]
-
-    s = N("S", 0.5, 0, 0)
-    t0 = N("T0", 0, 0, ts=ts[-1] + 1)
-    t1 = N("T1", 1, 1, ts=ts[-1] + 1)
-
-    al = [nodes0, nodes1]
-    return al, s, t0, t1
-
-
-class FIFOQueue:
-    def __init__(self):
-        self.q = []
-
-    def push(self, item):
-        self.q.append(item)
-
-    def pop(self):
-        return self.q.pop()
-
-    def empty(self):
-        return len(self.q) == 0
-
-    def __contains__(self, key):
-        return key in self.q
-
-
 @dataclass
 class Rsamp:
     k: float = field(default=1.35)
     La: float = field(default=1.19)
     _sigma: float = field(default=0.0, init=False)
+    _window: float = field(default=0.1, init=False)
 
     def emission_prob(self, n: N) -> float:
         c = 1 / (self._sigma * math.sqrt(2 * math.pi))
@@ -61,45 +31,37 @@ class Rsamp:
         delta = abs(n1.ts - n2.ts) / math.exp(self.k * kls)
         return self.La * math.exp(-delta)
 
-    def max_prob(self, adjacency_list, u, v, w):
+    def max_prob(self, u, v, w):
         prob0 = self.transition_prob(u, v) * self.emission_prob(v)
         prob1 = self.transition_prob(u, w) * self.emission_prob(w)
         return (v, prob0) if prob0 >= prob1 else (w, prob1)
 
-    def search(self, adjacency_list, s, t):
-        path = [s]
-        probs = []
+    def search(self, x, s):
         joint_prob = self.emission_prob(s)
-        records = len(adjacency_list[0])
+        records = len(x)
+        y_pred = np.zeros(records)
         _Node = s
 
         for i in range(records):
-            probs.append(joint_prob)
             u = _Node
-            v = adjacency_list[0][i]
-            w = adjacency_list[1][i]
-            _Node, new_prob = self.max_prob(adjacency_list, u, v, w)
-            path.append(_Node)
+            v = N("a", x[i], klass=0, ts=i+1)
+            w = N("b", x[i], klass=1, ts=i+1)
+            _Node, new_prob = self.max_prob(u, v, w)
+            y_pred[i] = _Node.klass
             joint_prob *= new_prob
 
-        _, new_prob = self.max_prob(adjacency_list, _Node, t, t)
-        joint_prob *= new_prob
+        return y_pred
 
-        path.append(t)
-        probs.append(joint_prob)
-
-        return joint_prob, path, probs
+    def predict_proba(self, xx):
+        num_samples = len(xx)
+        res = np.zeros(shape=(num_samples, 2))
+        for i in range(num_samples):
+            pred = self.predict(xx[i])
+            proba = (np.mean(pred) / self._window) % 1.0
+            res[i][0], res[i][1] = 1 - proba, proba
+        return res
 
     def predict(self, x):
         self._sigma = np.sqrt(np.std(x))
-        ts = list(range(1, len(x) + 1))
-
-        AL, s, t0, t1 = from_prob_linear(xx=x, ts=ts)
-        p0, c0, probs0 = self.search(AL, s, t0)
-        p1, c1, probs1 = self.search(AL, s, t1)
-
-        y_pred = [n.klass for n in c0[1:-1]]
-        return y_pred
-
-    def predict_all(self, xx):
-        return [self.predict(x) for x in xx]
+        s = N("S", 0.5, 0, 0)
+        return self.search(x, s)
